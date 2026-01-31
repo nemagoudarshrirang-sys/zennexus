f8f0efeb841b9e458b05649e225d9ae8b6c90f6c
Shrirang Nemagoudar <nemagoudarshrirang@gmail.com>
2026-01-31 20:41:02 +0530

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let studying = false;
+
+	function toggleStudy() {
+		studying = !studying;
+	}
+</script>
+
+<h1>ZenNexus</h1>
+
+<p>Status: {studying ? "Studying" : "Not Studying"}</p>
+
+<button on:click={toggleStudy}>
+	{studying ? "Stop Study" : "Start Study"}
+</button>
